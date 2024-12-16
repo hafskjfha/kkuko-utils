1d502010c8ff70d45e5a2983c68b117dd3738c91
Jung-Taewon <153927840+hafskjfha@users.noreply.github.com>
2024-12-16 14:04:38 +0000

    test

---
+++ app/hello/page.tsx
@@ -0,0 +1,10 @@
+// app/hello/page.tsx
+export default function HelloPage() {
+    return (
+      <div>
+        <h1>Hello, Turbopack!</h1>
+        <p>Welcome to the /hello route.</p>
+      </div>
+    );
+  }
+  
