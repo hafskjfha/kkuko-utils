d4bf73049c9ec28f34858a1ccc8d79c5649023a3
Jung-Taewon <153927840+hafskjfha@users.noreply.github.com>
2025-02-23 06:32:27 +0000

    한국어 미션 단어 추출 페이지 추가 및 관련 링크 수정

--- app/manager-tool/extract/ExtractHome.tsx
+++ app/manager-tool/extract/ExtractHome.tsx
@@ -10,9 +10,9 @@ const ExtractHome: React.FC = () => {
         { id: 3, name: "X로 끝나는 단어 추출", description: "텍스트 파일에서 X로 끝나는 단어들을 추출합니다", link: "/endx" },
         { id: 4, name: "돌림단어 추출", description: "텍스트 파일에서 돌림단어들을 추출합니다.", link: "/loop" },
         { id: 5, name: "파일합성", description: "2개의 텍스트파일을 합성합니다.", link: "/merge" },
-        { id: 6, name: "한국어 미션단어 추출", description: "텍스트 파일에서 한국어 미션단어들을 추출합니다.", link: "/korean-mission" },
+        { id: 6, name: "한국어 미션단어 추출 - A", description: "텍스트 파일에서 한국어 미션단어들을 추출합니다.", link: "/korean-mission" },
         { id: 7, name: "영어 미션단어 추출", description: "텍스트 파일에서 영어 미션단어들을 추출합니다.", link: "/english-mission" },
-        { id: 8, name: "기능고민중", description: "원하는 기능을 추가해달라고 요청해주세요!" },
+        { id: 8, name: "한국어 미션단어 추출 - B", description: "텍스트 파일에서 미션단어중 1티어만 추출합니다.", link: "/korean-mission-b" },
         { id: 9, name: "기능고민중", description: "원하는 기능을 추가해달라고 요청해주세요!" },
     ];
 
