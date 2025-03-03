eb4c31c8d72cd433ca0a111cd7f5f0339766f174
Jung-Taewon <153927840+hafskjfha@users.noreply.github.com>
2025-03-03 11:29:15 +0000

    ProfilePage 컴포넌트에 Suspense 추가하여 로딩 상태 처리 개선

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,12 @@
 import ProfilePage from "./ProfilePage";
+import { Suspense } from "react";
 
 export default function ProfilePageA() {
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <ProfilePage />
-    </div>
-  );
+    return (
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <Suspense fallback={<div>Loading...</div>}>
+                <ProfilePage />
+            </Suspense>
+        </div>
+    );
 }
