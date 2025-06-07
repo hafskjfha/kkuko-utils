c27258816e680db02340406b35e8a519bd63a23b
Jung-Taewon <153927840+hafskjfha@users.noreply.github.com>
2025-06-07 06:29:52 +0000

    유조 페이지 리펙토링 - lint 수정

--- app/profile/ProfileHome.tsx
+++ app/profile/ProfileHome.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useMemo } from "react";
+import React, { useState } from "react";
 import { Search, User, Crown, Star, Trophy, Loader2 } from "lucide-react";
 import { Card, CardContent, CardHeader } from "@/app/components/ui/card";
 import { Input } from "@/app/components/ui/input";
