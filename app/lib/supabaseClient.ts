bcfddffa096876df70bc3041192170829f9a06a6
jtw <jtw7913@gmail.com>
2025-06-15 22:00:16 +0900

    fuck

--- app/lib/supabaseClient.ts
+++ app/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createBrowserClient } from '@supabase/ssr';
 import type { Database } from '../types/database.types'
-import { SupabaseClientManager } from './supabase/SupabaseClient';
+import { SupabaseClientManager } from './supabase/SupabaseClientManager';
 
 export const supabase = createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
