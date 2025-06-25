c04ea0f22b032d8127262038b03717094d60f60b
Jung-Taewon <153927840+hafskjfha@users.noreply.github.com>
2025-06-25 09:34:25 +0000

    merge테스트 - 코드 수정

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' }, modules: 'auto' }],
-    '@babel/preset-typescript',
-    '@babel/preset-react', // 만약 누락됐다면 꼭 필요
-  ],
-}
